b3ded4d0f95f1991f82ada0018bd2cc8b09e0cd3
bbeck <bbeck@peoplenetonline.com>
2010-10-05 12:16:24 -0500

    Ignore unused warning...

--- consumer/src/main/java/jowambi/consumer/impl/SimpleConsumer.java
+++ consumer/src/main/java/jowambi/consumer/impl/SimpleConsumer.java
@@ -3,6 +3,7 @@ package jowambi.consumer.impl;
 import jowambi.service.ISimpleService;
 
 public class SimpleConsumer {
+	@SuppressWarnings("unused")
 	private ISimpleService service;
 	
 	public void setService(ISimpleService service) {
